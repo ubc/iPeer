f96f6ca5f771bbafe54c94f0bd74b22dd0c60817
Serge Okon <serge>
2010-05-18 00:22:31 +0000

    Updated the database definition file to access Turrito by IP and not localhost. This allows my local iPeer copy to connect to the database.

--- app/config/database.php
+++ app/config/database.php
@@ -2,7 +2,7 @@
 class DATABASE_CONFIG {
 var $default = array('driver'   => 'mysql',
                      'connect'  => 'mysql_pconnect',
-                     'host'     => 'localhost',
+                     'host'     => '137.82.12.110',
                      'login'    => 'ipeer',
                      'password' => '99bobotw',
                      'database' => 'ipeer',
