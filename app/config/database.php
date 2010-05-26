b80a58588862ee0a01d63924e4f207671bc4935c
Serge Okon <serge>
2010-05-26 22:21:54 +0000

    Added burrito and serge's laptop as database alternatives.

--- app/config/database.php
+++ app/config/database.php
@@ -1,10 +1,35 @@
 <?php
 class DATABASE_CONFIG {
-var $default = array('driver'   => 'mysql',
+
+// =-=-= The used Database =-=-=-=-=-=-=
+	
+var $default  = array('driver'   => 'mysql',
                      'connect'  => 'mysql_pconnect',
                      'host'     => '137.82.12.110',
                      'login'    => 'ipeer',
                      'password' => '99bobotw',
                      'database' => 'ipeer',
-                     'prefix'   => '');  }
+                     'prefix'   => '');  
+
+// =-=-=-= Avaliable Databases =-=-=-=-=-
+
+// The development server
+var $burrito = array('driver'   => 'mysql',
+                     'connect'  => 'mysql_pconnect',
+                     'host'     => '137.82.12.110',
+                     'login'    => 'ipeer',
+                     'password' => '99bobotw',
+                     'database' => 'ipeer',
+                     'prefix'   => '');  
+
+// Serge's laptop server
+var $sergeLaptop = array('driver'   => 'mysql',
+                     'connect'  => 'mysql_pconnect',
+                     'host'     => 'localhost',
+                     'login'    => 'ipeer',
+                     'password' => 'pass.word',
+                     'database' => 'ipeer',
+                     'prefix'   => '');  
+
+}
 ?>
