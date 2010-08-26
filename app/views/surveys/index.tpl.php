3b6bd17ed74a7130adfe11e6df306cfe4e106326
Serge Okon <serge>
2010-08-26 00:07:19 +0000

    Small fix for Surveys list.

--- app/views/surveys/index.tpl.php
+++ app/views/surveys/index.tpl.php
@@ -5,7 +5,7 @@
 <tr>
 <td align="center">
   <table width="95%" border="0" cellspacing="0" cellpadding="2">
-    <tr><td>
+    <tr><td style="text-align:right">
             <?php echo $html->image('icons/add.gif', array('alt'=>'Add Survey', 'align'=>'middle')); ?>&nbsp;<?php echo $html->linkTo('Add Survey', '/surveys/add/'); ?>
     </td></tr>
     <tr><td colspan=10>
