d2a2d72bb7613008fde1c7b68758450ae70dcf61
Serge Okon <serge>
2010-09-13 14:48:06 +0000

    Fixed a difference in handling of empty dates in iPeer. Close #219, #220.

--- app/webroot/js/events.js
+++ app/webroot/js/events.js
@@ -4,10 +4,33 @@
  function validateEventDates(eventBegin, eventEnd, EventDue) {
      // Get the proper event strings from Paramemters
 
+    var due = $(EventDue).value;
+    var release = $(eventBegin).value;
+    var finish = $(eventEnd).value;
+
+    // Warn in there are empty
+    if (!due) {
+        alert("Due date empty. Please Enter a valid date."); return false;
+    }
+
+    if (!release) {
+        alert("Release From date empty. Please Enter a valid date."); return false;
+    }
+
+    if (!finish) {
+        alert("Release To date empty. Please Enter a valid date."); return false;
+    }
+
+
+    var dueDate = Date.parse(due.replace(/-/g,"/"));
+    var releaseDate = Date.parse(release.replace(/-/g,"/"));
+    var finishDate = Date.parse(finish.replace(/-/g,"/"));
+
      // Get the dates from the field in a milisecond number format
-     var dueDate = Date.parse($(EventDue).value.replace(/-/g,"/"));
-     var releaseDate = Date.parse($(eventBegin).value.replace(/-/g,"/"));
-     var finishDate = Date.parse($(eventEnd).value.replace(/-/g,"/"));
+
+    var dueDate = Date.parse($(EventDue).value.replace(/-/g,"/"));
+    var releaseDate = Date.parse($(eventBegin).value.replace(/-/g,"/"));
+    var finishDate = Date.parse($(eventEnd).value.replace(/-/g,"/"));
 
      // First, check for valid entries
      if (isNaN(dueDate)) {
