944646b50cb385a76df4be881c4c515db00c8372
John Hsu <john.hsu@ubc.ca>
2012-07-17 15:42:40 -0700

    API controller now has a blank layout.
    It needs to return only plain text output, so it doesn't need all the
    CSS stuff.

--- app/controllers/v1_controller.php
+++ app/controllers/v1_controller.php
@@ -1,6 +1,7 @@
 <?php
 class V1Controller extends Controller {
     public $uses = array();
+    public $layout = "blank_layout";
     
     public function users() {
         $this->set('test', 'test');
