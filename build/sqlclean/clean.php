2c7bccdf2dca7cdb81dccfe6934e5383f7961496
John Hsu <john.hsu@ubc.ca>
2012-07-17 11:08:37 -0700

    Fix test case fail due to no superadmin.
    Forgot that the superadmin user was created by the installer, not by
    ipeer_sample_data.sql. Some of the test cases uses the admin user and so
    fails.

--- build/sqlclean/clean.php
+++ build/sqlclean/clean.php
@@ -1,3 +1,4 @@
 <?php
 $ret = shell_exec('mysql -u root < clean.sql');
 $ret = shell_exec('mysql -u root database_name < ../../app/config/sql/ipeer_samples_data.sql');
+$ret = shell_exec('mysql -u root database_name < superadmin.sql');
---
+++ build/sqlclean/superadmin.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `student_no`, `title`, `email`, `last_login`, `last_logout`, `last_accessed`, `record_status`, `creator_id`, `created`, `updater_id`, `modified`, `lti_id`) VALUES
+(1, 'root', 'cbbde255cc20cb7a566d4c7f12298729', 'Super', 'Admin', NULL, NULL, '', NULL, NULL, NULL, 'A', 0, '2012-07-17 11:01:37', NULL, NULL, NULL);
+
