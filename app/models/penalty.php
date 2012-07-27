b90190b92080c598e7ef4ae40136393378d000f7
Michael Tang <michael.tang@ubc.ca>
2012-07-27 11:30:43 -0700

    fixed the radio button's css for emailtemplates/add
    Before, the radio buttons & label were not aligned with the rest
    of the fields of the form. We have fixed it by clearing the left
    and right of the fieldset - which caused the indentation, that we
    don't want.
    
    I have also removed the commented out/old code from the penalty
    model.

--- app/models/penalty.php
+++ app/models/penalty.php
@@ -39,7 +39,6 @@ class Penalty extends AppModel
      */
     function getPenaltyByEventId($eventId)
     {
-        //return $this->find('all', array('conditions' => array('Penalty.event_id' => $eventId, 'Penalty.days_late >' => 0), 'order' => array('Penalty.days_late')));
         $penalties = $this->find('all', array('conditions' => array('Penalty.event_id' => $eventId), 'order' => array('Penalty.days_late')));
         // pop off the final deduction
         array_pop($penalties);
@@ -57,7 +56,6 @@ class Penalty extends AppModel
      */
     function getPenaltyFinal($eventId)
     {
-        //return $this->find('first', array('conditions' => array('Penalty.event_id' => $eventId, 'Penalty.days_late <' => 0)));
         $final = $this->find('all', array('conditions' => array('Penalty.event_id' => $eventId)));
         return array_pop($final);
     }
@@ -73,7 +71,6 @@ class Penalty extends AppModel
      */
     function getPenaltyDays($eventId)
     {
-        // subtract the final deduction from the count
         $count = $this->find('count',
             array('conditions'=>array('Penalty.event_id' => $eventId)));
         if ($count > 0) {
