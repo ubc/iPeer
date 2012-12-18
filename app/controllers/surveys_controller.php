5828b1b7b264527200ce1a203b09e53467b2b930
Compass <pan.luo@ubc.ca>
2012-12-18 11:40:45 -0800

    Fix the syntax error

--- app/controllers/surveys_controller.php
+++ app/controllers/surveys_controller.php
@@ -261,7 +261,7 @@ class SurveysController extends AppController
                 }
             }
             if (!empty($submissions)) {
-                $this->Session->setFlash(sprintf(__('Submissions had been made. %s cannot be edited. Please make a copy.', true), $survey['Survey']['name']);
+                $this->Session->setFlash(sprintf(__('Submissions had been made. %s cannot be edited. Please make a copy.', true), $survey['Survey']['name']));
                 $this->redirect('index');
             }
         }
