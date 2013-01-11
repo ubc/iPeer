14d0d0383700ede518215d1bf36ef2720a5e8e43
Michael Tang <michael.tang@ubc.ca>
2013-01-11 10:33:24 -0800

    Fix #469
    Users with the same student number, first, and last name can now
    be added without one being excluded and the other being put into
    the group twice.

--- app/webroot/js/groups.js
+++ app/webroot/js/groups.js
@@ -6,20 +6,20 @@ function itemMove(fbox, tbox) {
 	var i;
 
 	for (i = 0; i < tbox.options.length; i++) {
-		arrLookup[tbox.options[i].text] = tbox.options[i].value;
-		arrTbox[i] = tbox.options[i].text;
+		arrLookup[tbox.options[i].value] = tbox.options[i].text;
+		arrTbox[i] = tbox.options[i].value;
 	}
 
 	var fLength = 0;
 	var tLength = arrTbox.length;
 	for(i = 0; i < fbox.options.length; i++) {
-		arrLookup[fbox.options[i].text] = fbox.options[i].value;
+		arrLookup[fbox.options[i].value] = fbox.options[i].text;
 		if (fbox.options[i].selected && fbox.options[i].value != "") {
-			arrTbox[tLength] = fbox.options[i].text;
+			arrTbox[tLength] = fbox.options[i].value;
 			tLength++;
 		}
 		else {
-			arrFbox[fLength] = fbox.options[i].text;
+			arrFbox[fLength] = fbox.options[i].value;
 			fLength++;
 		}
 	}
@@ -32,14 +32,14 @@ function itemMove(fbox, tbox) {
 
 	for(c = 0; c < arrFbox.length; c++) {
 		var no = new Option();
-		no.value = arrLookup[arrFbox[c]];
-		no.text = arrFbox[c];
+		no.value = arrFbox[c];
+		no.text = arrLookup[arrFbox[c]];
 		fbox[c] = no;
 	}
 	for(c = 0; c < arrTbox.length; c++) {
 		var no = new Option();
-		no.value = arrLookup[arrTbox[c]];
-		no.text = arrTbox[c];
+		no.value = arrTbox[c];
+		no.text = arrLookup[arrTbox[c]];
 		tbox[c] = no;
 	}
 
