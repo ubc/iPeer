9bc12f9d9e2c9ad9e6d9ae04cf48e23740651c32
Compass <pan.luo@ubc.ca>
2013-01-11 15:39:58 -0800

    Fix incorrect logging when sending submission email failed.

--- app/controllers/evaluations_controller.php
+++ app/controllers/evaluations_controller.php
@@ -355,7 +355,7 @@ class EvaluationsController extends AppController
         }
 
         if (!$this->TemplateEmail->send(array(User::get('id') => $email), 'Submission Confirmation')) {
-            $this->log('Sending email to '.$email.' failed.'. $this->Email->smtpError);
+            $this->log('Sending email to '.$email.' failed.'. $this->TemplateEmail->smtpError);
             $this->Session->setFlash('Sending confirmation email failed!');
         }
     }
