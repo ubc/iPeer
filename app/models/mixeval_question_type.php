526d9f53a53b2e5c507f310d0df6bed2d6791afd
John Hsu <john.hsu@ubc.ca>
2013-02-19 18:05:21 -0800

    Fix tab indent in MixevalQuestionType model.
    It was using tab indent due to it being autogenerated by cake bake.

--- app/models/mixeval_question_type.php
+++ app/models/mixeval_question_type.php
@@ -1,35 +1,35 @@
 <?php
 class MixevalQuestionType extends AppModel {
-	var $name = 'MixevalQuestionType';
-	var $displayField = 'type';
-	var $validate = array(
-		'type' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-	);
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+    var $name = 'MixevalQuestionType';
+    var $displayField = 'type';
+    var $validate = array(
+        'type' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+    );
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
 
-	var $hasMany = array(
-		'MixevalQuestion' => array(
-			'className' => 'MixevalQuestion',
-			'foreignKey' => 'mixeval_question_type_id',
-			'dependent' => false,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'exclusive' => '',
-			'finderQuery' => '',
-			'counterQuery' => ''
-		)
-	);
+    var $hasMany = array(
+        'MixevalQuestion' => array(
+            'className' => 'MixevalQuestion',
+            'foreignKey' => 'mixeval_question_type_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        )
+    );
 
 }
