53fba5aa85f9579548eacc5b52c58816ca9601ba
Compass <pan.luo@ubc.ca>
2013-02-28 16:15:24 -0800

    Add db setting for pagoda

---
+++ app/config/database.pagoda.php
@@ -0,0 +1,19 @@
+<?php
+define("DB_HOST", $_SERVER['DB1_HOST']);
+define("DB_NAME", $_SERVER['DB1_NAME']);
+define("DB_USER", $_SERVER['DB1_USER']);
+define("DB_PASS", $_SERVER['DB1_PASS']);
+
+class DATABASE_CONFIG
+        {
+public $default = array(
+    'driver' => 'mysql',
+    'connect' => 'mysql_pconnect',
+    'host'   => DB_HOST,
+    'login'   => DB_USER,
+    'password'   => DB_PASS,
+    'database'   => DB_NAME,
+    'prefix'   => ''
+  );
+}
+?>
