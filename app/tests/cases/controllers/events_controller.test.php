f17eca71395df13446dc3138f9b149feb80b5b82
mwytang <michael.tang@ubc.ca>
2013-03-06 16:54:15 -0800

    included tests for enable_details field for events controller

--- app/tests/cases/controllers/events_controller.test.php
+++ app/tests/cases/controllers/events_controller.test.php
@@ -326,6 +326,7 @@ class EventsControllerTest extends ExtendedAuthTestCase {
                 'SimpleEvaluation' => 1,
                 'self_eval' => 0,
                 'com_req' => 0,
+                'enable_details' => 1,
                 'due_date' => '2012-11-28 00:00:01',
                 'release_date_begin' => '2012-11-20 00:00:01',
                 'release_date_end' => '2012-11-29 00:00:01',
