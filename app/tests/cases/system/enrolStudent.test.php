b93ad03df72b29639ce54bfd0b1d9ed328f758ef
mwytang <michael.tang@ubc.ca>
2013-04-02 14:40:53 -0700

    system test for manually enrolling a student
    When a student already exists in the system, the user can enrol
    the student via a link that appears along with the duplicate
    username warning. This new minor feature is being tested in this
    test.

---
+++ app/tests/cases/system/enrolStudent.test.php
@@ -0,0 +1,91 @@
+<?php
+require_once('PHPWebDriver/WebDriver.php');
+require_once('PHPWebDriver/WebDriverBy.php');
+require_once('PHPWebDriver/WebDriverWait.php');
+require_once('PHPWebDriver/WebDriverSession.php');
+require_once('PageFactory.php');
+
+class EnrolStudentTestCase extends CakeTestCase
+{
+    protected $web_driver;
+    protected $session;
+    protected $url = 'http://ipeerdev.ctlt.ubc.ca/';
+    
+    public function startCase()
+    {
+        $wd_host = 'http://localhost:4444/wd/hub';
+        $this->web_driver = new PHPWebDriver_WebDriver($wd_host);
+        $this->session = $this->web_driver->session('firefox');
+        $this->session->open($this->url);
+        
+        $w = new PHPWebDriver_WebDriverWait($this->session);
+        $this->session->deleteAllCookies();
+        $login = PageFactory::initElements($this->session, 'Login');
+        $home = $login->login('root', 'ipeeripeer');
+    }
+    
+    public function endCase()
+    {
+        $this->session->deleteAllCookies();
+        $this->session->close();
+    }
+    
+    public function testEnrolStudent()
+    {
+        $this->session->open($this->url.'users/add/2');
+        $title = $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "h1.title")->text();
+        $this->assertEqual($title, 'APSC 201 - Technical Communication > Add User');
+        
+        // enter username: redshirt0004
+        $this->session->element(PHPWebDriver_WebDriverBy::ID, 'UserUsername')->sendKeys('redshirt0004');
+        // wait for "username already exist" warning
+        $w = new PHPWebDriver_WebDriverWait($this->session);
+        $session = $this->session;
+        $w->until(
+            function($session) {
+                return $session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'div[id="usernameErr"]')->text();
+            }
+        );
+        
+        $warning = $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'div[id="usernameErr"]')->text();
+        $this->assertEqual(substr($warning, 0, 39), 'Username "redshirt0004" already exists.');
+        
+        // click here to enrol
+        $this->session->element(PHPWebDriver_WebDriverBy::LINK_TEXT, 'here')->click();
+        
+        // wait for the student to be enrolled
+        $w->until(
+            function($session) {
+                return count($session->elements(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='message good-message green']"));
+            }
+        );
+        
+        $msg = $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='message good-message green']")->text();
+        $this->assertEqual($msg, 'Student is successfully enrolled.');  
+    }
+    
+    public function testUnenrolStudent()
+    {
+        // unenrol Chris Student
+        $this->session->open($this->url.'users/goToClassList/2');
+        $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'input[aria-controls="table_id"]')->sendKeys('Chris');
+        $w = new PHPWebDriver_WebDriverWait($this->session);
+        $session = $this->session;
+        $w->until(
+            function($session) {
+                $count = count($session->elements(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'tr[class="odd"]'));
+                return ($count == 1);
+            }
+        );
+        $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'td[class="  sorting_1"]')->click();
+        $this->session->element(PHPWebDriver_WebDriverBy::LINK_TEXT, 'Drop')->click();
+        $this->session->accept_alert();
+        $w->until(
+            function($session) {
+                return count($session->elements(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='message good-message green']"));
+            }
+        );
+        $msg = $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='message good-message green']")->text();
+        $this->assertEqual($msg, 'Student is successfully unenrolled!');
+    }
+}
