0381dc9dc2c84804a1a193c7df7afb4289867acb
mwytang <michael.tang@ubc.ca>
2013-04-08 15:17:37 -0700

    created system tests for creating groups (auto) - TeamMaker

---
+++ app/tests/cases/system/autoCreateGroups.test.php
@@ -0,0 +1,140 @@
+<?php
+require_once('PHPWebDriver/WebDriver.php');
+require_once('PHPWebDriver/WebDriverBy.php');
+require_once('PHPWebDriver/WebDriverWait.php');
+require_once('PageFactory.php');
+
+class autoCreateGroups extends CakeTestCase
+{
+    protected $web_driver;
+    protected $session;
+    protected $url = 'http://ipeerdev.ctlt.ubc.ca/';
+    protected $surveyGroupId = 0;
+
+    public function startCase()
+    {
+        $wd_host = 'http://localhost:4444/wd/hub';
+        $this->web_driver = new PHPWebDriver_WebDriver($wd_host);
+        $this->session = $this->web_driver->session('firefox');
+        $this->session->open($this->url);
+        
+        $w = new PHPWebDriver_WebDriverWait($this->session);
+        $this->session->deleteAllCookies();
+        $login = PageFactory::initElements($this->session, 'Login');
+        $home = $login->login('root', 'ipeeripeer');
+    }
+    
+    public function endCase()
+    {
+        $this->session->deleteAllCookies();
+        $this->session->close();
+    }
+    
+    public function testStepOne()
+    {
+        $this->session->open($this->url.'courses/home/1');
+        $this->session->element(PHPWebDriver_WebDriverBy::LINK_TEXT, 'Create Groups (Auto)')->click();
+        $title = $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "h1.title")->text();
+        $this->assertEqual($title, 'MECH 328 - Mechanical Engineering Design Project > Create Group Set');
+        // choose survey
+        $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'select[id="event_select"] option[value="4"]')->click();
+        
+        $w = new PHPWebDriver_WebDriverWait($this->session);
+        $session = $this->session;
+        $w->until(
+            function($session) {
+                $step = $session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'th');
+                return ($step->text() == 'Team Making - Step One');
+            }
+        );
+        
+        // choose group configuration - chose to have 6 groups
+        $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'select[id="group_config"] option[value="6"]')->click();
+        // weights for each survey question
+        $weights3 = $this->session->elements(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'input[value="3"]');
+        $weights3[0]->click();   // 1st question
+        $weights3[1]->click();   // 2nd question
+        $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'input[value="Next"]')->click();
+    }
+    
+    public function testStepTwo()
+    {
+        $title = $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "h1.title")->text();
+        $this->assertEqual($title, 'Survey Groups > Teams Summary');
+        $numGroups = count($this->session->elements(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "tr")) - 1;
+        $this->assertEqual($numGroups, 6);
+        $students = count($this->session->elements(PHPWebDriver_WebDriverBy::PARTIAL_LINK_TEXT, "Student"));
+        $this->assertEqual($students, 13);
+        $this->session->element(PHPWebDriver_WebDriverBy::ID, 'team_set_name')->sendKeys('Great Group');
+        $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'input[value="Save Groups"]')->click();
+    }
+    
+    public function testEditGroups()
+    {
+        $this->session->element(PHPWebDriver_WebDriverBy::LINK_TEXT, 'Team Creation Survey')->click();
+        $this->surveyGroupId = end(explode('/', $this->session->url()));
+        $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'select[id="question"] option[value="1"]')->click();
+        $ques1Ans = count($this->session->elements(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'input[checked]'));
+        $this->assertEqual($ques1Ans, 2);
+        $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'select[id="question"] option[value="2"]')->click();
+        $ques2Ans = count($this->session->elements(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'input[checked]'));
+        $this->assertEqual($ques2Ans, 2);
+
+        $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, 'input[value="Save Groups"]')->click();
+        $w = new PHPWebDriver_WebDriverWait($this->session);
+        $session = $this->session;
+        $w->until(
+            function($session) {
+                return count($session->elements(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='message good-message green']"));
+            }
+        );
+        $msg = $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='message good-message green']")->text();
+        $this->assertEqual($msg, 'Group set changed successfully.');
+    }
+    
+    public function testReleaseGroups()
+    {
+        $this->session->open($this->url.'surveygroups/release/'.$this->surveyGroupId);
+        $w = new PHPWebDriver_WebDriverWait($this->session);
+        $session = $this->session;
+        $w->until(
+            function($session) {
+                return count($session->elements(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='message good-message green']"));
+            }
+        );
+        $msg = $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='message good-message green']")->text();
+        $this->assertEqual($msg, 'The group set was released successfully.');
+        
+        $this->session->open($this->url.'groups/index/1');
+        $groups = count($this->session->elements(PHPWebDriver_WebDriverBy::PARTIAL_LINK_TEXT, 'Great Group Team #'));
+        $this->assertEqual($groups, 6);
+    }
+    
+    public function testDeleteGroups()
+    {
+        $w = new PHPWebDriver_WebDriverWait($this->session);
+        $session = $this->session;
+        // delete the 6 groups
+        for ($i=0; $i<6; $i++) {
+            $this->session->element(PHPWebDriver_WebDriverBy::PARTIAL_LINK_TEXT, 'Great Group Team #')->click();
+            $this->session->open(str_replace('view', 'delete', $this->session->url()));
+            $w->until(
+                function($session) {
+                    return count($session->elements(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='message good-message green']"));
+                }
+            );
+            $msg = $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='message good-message green']")->text();
+            $this->assertEqual($msg, 'The group was deleted successfully.'); 
+        }
+        
+        // delete the survey group set
+        $this->session->open($this->url.'surveygroups/delete/'.$this->surveyGroupId);
+        $w->until(
+            function($session) {
+                return count($session->elements(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='message good-message green']"));
+            }
+        );
+        $msg = $this->session->element(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='message good-message green']")->text();
+        $this->assertEqual($msg, 'The group set was deleted successfully.'); 
+    }
+}
