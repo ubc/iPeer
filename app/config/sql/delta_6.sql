811095b522c8527366ec40bb6f93f7b98fd5dd3b
Pan Luo <pan.luo@ubc.ca>
2014-11-04 16:26:52 -0800

    Move delta 5 into delta 4 and remove sql mode in 6
    The database versions between 3.0 to 3.1 are all 4. So delta_5 should be
    merged into 4

--- app/config/sql/delta_6.sql
+++ app/config/sql/delta_6.sql
@@ -1,6 +1,5 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
 -- Update datetime for all tables
 
@@ -835,6 +834,5 @@ INSERT INTO aros_acos (id, aro_id, aco_id, _create, _read, _update, _delete) VAL
 
 -- --------------------------------------------------------
 
-SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
