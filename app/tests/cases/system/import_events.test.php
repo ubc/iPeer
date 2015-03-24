5ba5ba79d99542249a0627c9f7f977c0a4c8242b
Pan Luo <pan.luo@ubc.ca>
2015-03-23 20:20:03 -0700

    Add missing fixtures

--- app/tests/cases/system/import_events.test.php
+++ app/tests/cases/system/import_events.test.php
@@ -18,23 +18,22 @@ class importEventsTestCase extends SystemBaseTestCase
 
     public function testImportSuccess()
     {
-        return;
         $this->session->open($this->url.'events/import/1');
 
         $file = $this->session->elementWithWait(PHPWebDriver_WebDriverBy::ID, 'ImportEventsFile');
         $file->sendKeys(dirname(__FILE__).'/files/import-test-success.csv');
 
         $this->session->elementWithWait(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='submit'] input")->click();
-        
+
         $msg = $this->session->elementWithWait(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='message good-message green']")->text();
         $this->assertEqual($msg, "4 event(s) were imported successfully.");
-        
+
         $this->session->elementWithWait(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "option[value='Event.Title']")->click();
         $this->session->elementWithWait(PHPWebDriver_WebDriverBy::ID, "searchInputField")->sendKeys('test-event-1');
-        
+
         // link to event should exist
         $this->session->elementWithWait(PHPWebDriver_WebDriverBy::LINK_TEXT, 'test-event-1')->click();
-        
+
         // link to group should exist
         $this->session->elementWithWait(PHPWebDriver_WebDriverBy::LINK_TEXT, '1')->click();
     }
@@ -49,7 +48,7 @@ class importEventsTestCase extends SystemBaseTestCase
         $this->session->elementWithWait(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='submit'] input")->click();
         $msg = $this->session->elementWithWait(PHPWebDriver_WebDriverBy::ID, 'flashMessage')->text();
         $this->assertEqual($msg, "Import Failed\nEvent \"test-event-4\" (on row 4), has an invalid field: \"Type\": Please select a template type.");
-        
+
         // wrong extension
         $this->session->open($this->url.'events/import/1');
         $file = $this->session->elementWithWait(PHPWebDriver_WebDriverBy::ID, 'ImportEventsFile');
@@ -57,6 +56,6 @@ class importEventsTestCase extends SystemBaseTestCase
         $this->session->elementWithWait(PHPWebDriver_WebDriverBy::CSS_SELECTOR, "div[class='submit'] input")->click();
         $msg = $this->session->elementWithWait(PHPWebDriver_WebDriverBy::ID, 'flashMessage')->text();
         $this->assertEqual($msg, "extension is not allowed.\nFileUpload::processFile() - Unable to save temp file to file system.");
- 
+
     }
 }
