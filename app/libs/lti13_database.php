534cdacd581a5020e02730259d7273a1cc17800d
Steven Marshall <steven.marshall@ubc.ca>
2019-12-12 16:23:02 -0800

    Corrected the 'glob' path in Lti13Database::set_issuers().

--- app/libs/lti13_database.php
+++ app/libs/lti13_database.php
@@ -24,7 +24,7 @@ class Lti13Database implements LTI_Database {
      */
     private function set_issuers()
     {
-        $filenames = glob(ROOT.DS.'app'.DS.'config'.DS.'lti13'.DS.'*.json');
+        $filenames = glob(ROOT.'/app/config/lti13/*.json');
         foreach ($filenames as $filename) {
             $json = json_decode(file_get_contents($filename), true);
             $this->issuers = array_merge($this->issuers, $json);
