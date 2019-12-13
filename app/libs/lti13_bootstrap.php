a9dab4def48e077ccb7eddea2f13dc0d70b38798
Steven Marshall <steven.marshall@ubc.ca>
2019-12-12 16:00:48 -0800

    Added isset condition to $_SERVER['HTTP_X_FORWARDED_PROTO'] in bootstrap.

--- app/libs/lti13_bootstrap.php
+++ app/libs/lti13_bootstrap.php
@@ -6,7 +6,9 @@
  * then by loading IMSGlobal\LTI Composer package
  * at the top of every LTI13-related file.
  */
-$_SERVER['HTTP_X_FORWARDED_PROTO'] = env('HTTP_X_FORWARDED_PROTOCOL');
+if (!isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
+    $_SERVER['HTTP_X_FORWARDED_PROTO'] = env('HTTP_X_FORWARDED_PROTOCOL');
+}
 require_once ROOT.DS.'vendor'.DS.'fproject'.DS.'php-jwt'.DS.'src'.DS.'JWT.php';
 require_once ROOT.DS.'vendor'.DS.'fproject'.DS.'php-jwt'.DS.'src'.DS.'JWK.php';
 require_once ROOT.DS.'vendor'.DS.'imsglobal'.DS.'lti-1p3-tool'.DS.'src'.DS.'lti'.DS.'lti.php';
