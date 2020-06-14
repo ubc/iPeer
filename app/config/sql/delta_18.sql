d4181e52de06c4897c9e812afe353161d1710758
Steven Marshall <steven.marshall@ubc.ca>
2020-06-14 15:54:17 -0700

    Deleted INSERT statements in delta_18.sql.

--- app/config/sql/delta_18.sql
+++ app/config/sql/delta_18.sql
@@ -11,12 +11,6 @@ CREATE TABLE `lti_platform_deployments` (
   KEY `iss` (`iss`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
-INSERT INTO `lti_platform_deployments` VALUES
-('https://lti-ri.imsglobal.org', '1'),
-('https://canvas.instructure.com', '1:4dde05e8ca1973bcca9bffc13e1548820eee93a3'),
-('https://canvas.instructure.com', '2:f97330a96452fc363a34e0ef6d8d0d3e9e1007d2'),
-('https://canvas.instructure.com', '3:d3a2504bba5184799a38f141e8df2335cfa8206d');
-
 DROP TABLE IF EXISTS `lti_tool_registrations`;
 CREATE TABLE `lti_tool_registrations` (
   `iss` varchar(255) NOT NULL,
@@ -27,21 +21,3 @@ CREATE TABLE `lti_tool_registrations` (
   `tool_private_key_file` varchar(255) NOT NULL,
   PRIMARY KEY `iss` (`iss`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-
-INSERT INTO `lti_tool_registrations` VALUES
-(
-  'https://lti-ri.imsglobal.org',
-  'ipeer-lti13-001',
-  'https://lti-ri.imsglobal.org/platforms/652/authorizations/new',
-  'https://lti-ri.imsglobal.org/platforms/652/access_tokens',
-  'https://lti-ri.imsglobal.org/platforms/652/platform_keys/654.json',
-  'app/config/lti13/tool.private.key'
-),
-(
-  'https://canvas.instructure.com',
-  '10000000000001',
-  'http://canvas.docker/api/lti/authorize_redirect',
-  'http://canvas.docker/login/oauth2/token',
-  'http://canvas.docker/api/lti/security/jwks',
-  'app/config/lti13/tool.private.key'
-);
