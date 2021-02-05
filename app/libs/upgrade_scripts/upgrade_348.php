4b0e466c76df5ff32cdbc74f9ef80d8f2c7e29e9
Pan Luo <pan.luo@ubc.ca>
2021-02-04 21:48:07 -0800

    Release version 3.4.8

---
+++ app/libs/upgrade_scripts/upgrade_348.php
@@ -0,0 +1,48 @@
+<?php
+require_once('upgrade_base.php');
+/**
+ * Upgrade347
+ *
+ * @uses UpgradeBase
+ * @package   CTLT.iPeer
+ * @author    Pan Luo
+ * @copyright 2021 All rights reserved.
+ * @license   PHP Version 3.0 {@link http://www.php.net/license/3_0.txt}
+ * @version   Release: 3.4.8
+ */
+ class Upgrade348 extends UpgradeBase
+{
+    /**
+     * __construct
+     *
+     * @access public
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->fromVersions = array(null, '3.4.7');
+        $this->toVersion = '3.4.8';
+        $this->dbVersion = 17;
+    }
+
+    /**
+     *
+     * @access public
+     * @return bool
+     */
+    public function isUpgradable()
+    {
+        return parent::isUpgradable();
+    }
+
+    /**
+     * up
+     *
+     * @access public
+     * @return boolean
+     */
+    public function up()
+    {
+        return true;
+    }
+}
--- readme.md
+++ readme.md
@@ -201,6 +201,11 @@ You may optionally override the user default IRI (from `$base_url/users/view/$us
 
 `CALIPER_ACTOR_UNIQUE_IDENTIFIER_PARAM`: Optionally set the actor's unique identifier using any column from the `user` table (ex: `username`, `id`, `email`). Will be inserted into the `CALIPER_ACTOR_BASE_URL` string.
 
+iPeer 3.4.8
+-----------
+* Fix a bug preventing instructor adding survey based event
+
+
 iPeer 3.4.7
 -----------
 * Increase PHP memory limit for docker container
