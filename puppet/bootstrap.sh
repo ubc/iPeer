70fbdc845c841145d1c15622661de82dabdb20a6
Alex Gondek <alex.gondek@ubc.ca>
2026-01-21 14:18:27 -0800

    Remove puppet and vagrant configuration (no longer supported)

--- puppet/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-# install phing
-pear config-set auto_discover 1
-pear channel-discover pear.phing.info
-pear upgrade Archive_Tar
-pear install phing/phing
-
-# restart server
-sudo service php-fpm restart
