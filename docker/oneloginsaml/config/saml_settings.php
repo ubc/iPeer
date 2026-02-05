87054e53a98e09c3fe9b8e371bb7f0ee0c649e6a
Alex Gondek <alex.gondek@ubc.ca>
2026-02-05 14:34:54 -0800

    Cleanup saml auth sidecar image contents, enable apache prod settings

--- docker/oneloginsaml/config/saml_settings.php
+++ docker/oneloginsaml/config/saml_settings.php
@@ -1,15 +1,7 @@
 <?php
-// config/saml_settings.php
 
 if (!empty($_ENV['SAML_SETTINGS'])) {
-    $modifiedString = str_replace(['{', '}'], ['[', ']'], $_ENV['SAML_SETTINGS']);
-
-    $modifiedArray = json_decode($_ENV['SAML_SETTINGS'], true);
-
-    return $modifiedArray;
-
-} else {
-
-    return [];
-
+    return json_decode($_ENV['SAML_SETTINGS'], true);
 }
+
+return [];
--- docker/oneloginsaml/public/saml/auth.php
+++ docker/oneloginsaml/public/saml/auth.php
@@ -1,17 +1,14 @@
 <?php
-// public/saml/auth.php 
+
 require_once '../../vendor/autoload.php';
 use OneLogin\Saml2\Auth;
 
-// Initialize SAML authentication
 $samlSettings = require '../../config/saml_settings.php';
 $auth = new Auth($samlSettings);
 
-// If user is logged in, redirect to dashboard
 if ($auth->isAuthenticated()) {
     header('Location: /dashboard');
     exit;
 }
 
-// Perform SAML login
 $auth->login();
