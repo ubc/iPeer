4db61302fe52e67cb661c5c273a5f1f51c7d80b6
Alex Gondek <alex.gondek@ubc.ca>
2026-05-11 12:16:25 -0700

    Update app/libs/toolkit.php
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- app/libs/toolkit.php
+++ app/libs/toolkit.php
@@ -103,7 +103,9 @@ class Toolkit
         $ret = array();
         if (($handle = fopen($file, "r")) !== false) {
             $bom = fread($handle, 3); # check for and strip byte order marker (BOM) if present
-            if ($bom !== "\xEF\xBB\xBF") {
+            if ($bom === false) {
+                trigger_error('Error reading file ' . $file, E_USER_ERROR);
+            } elseif ($bom !== "\xEF\xBB\xBF") {
                 rewind($handle);
             }
             while (($data = fgetcsv($handle, 1000, ",")) !== false) {
